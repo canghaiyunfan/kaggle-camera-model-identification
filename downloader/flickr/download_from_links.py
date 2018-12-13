4e011f5e8757932ddc37d6c073fde9d412288f70
zhaoyi <huashuizhaoyi@163.com>
2018-12-13 22:14:34 +0800

    原代码无法跑通

--- downloader/flickr/download_from_links.py
+++ downloader/flickr/download_from_links.py
@@ -3,16 +3,24 @@ import pandas as pd
 import httplib2
 from bs4 import BeautifulSoup
 import urllib.request
-from tqdm import tqdm
+#from tqdm import tqdm
 import os
 
 
 folders = os.listdir('links/')
 for i in folders:
     cnt = 0
-    pd.read_csv('links/'+i)
-    img = urllib.request.urlopen(img_url).read()
-    out = open("files/{}/{}.jpg".format(i,cnt), "wb")
-    out.write(img)
-    out.close
+    img_url_strs = []
+    img_url = pd.read_csv('links/'+i)
+    img_url_str = img_url.applymap(str)
+    with open("files/{}/{}.jpg".format(i[0:-4], cnt), "wb") as out:
+        for index in range(img_url.shape[0]):
+            #发现链接只能是str类型，list不可以
+            img = urllib.request.urlopen(img_url_str.iloc[index,0]).read()
+            print("download folders:{},index:{},length:{},link:{}".format(i[0:-4],index,
+                                                                  img_url.shape[0],
+                                                                  img_url_str.iloc[index,0]))
+            out.write(img)
     cnt += 1
+
+print("finished!!!")
