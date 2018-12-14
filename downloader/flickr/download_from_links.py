5705c0a8e5a0fee60cd0811574f0697cfa4e485c
zhaoyi <huashuizhaoyi@163.com>
2018-12-14 09:27:24 +0800

    修改智能保存一张图片的问题

--- downloader/flickr/download_from_links.py
+++ downloader/flickr/download_from_links.py
@@ -13,14 +13,14 @@ for i in folders:
     img_url_strs = []
     img_url = pd.read_csv('links/'+i)
     img_url_str = img_url.applymap(str)
-    with open("files/{}/{}.jpg".format(i[0:-4], cnt), "wb") as out:
-        for index in range(img_url.shape[0]):
-            #发现链接只能是str类型，list不可以
-            img = urllib.request.urlopen(img_url_str.iloc[index,0]).read()
-            print("download folders:{},index:{},length:{},link:{}".format(i[0:-4],index,
-                                                                  img_url.shape[0],
-                                                                  img_url_str.iloc[index,0]))
+    for index in range(img_url.shape[0]):
+        # 发现链接只能是str类型，list不可以
+        img = urllib.request.urlopen(img_url_str.iloc[index, 0]).read()
+        with open("files/{}/{}.jpg".format(i[0:-4], i[0:-4] + '_' + str(cnt)), "wb") as out:
+            print("download folders:{},index:{},length:{},link:{}".format(i[0:-4], index,
+                                                                      img_url.shape[0],
+                                                                      img_url_str.iloc[index, 0]))
             out.write(img)
-    cnt += 1
+        cnt += 1
 
 print("finished!!!")
